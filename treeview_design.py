b1b7b08395861829088af8225080af73996fd533
khawlahali2 <162768237+khawlahali2@users.noreply.github.com>
2024-03-29 15:03:55 -0400

    re-designed the form
    re-designed the form by presenting column's/headlines vertically, added entries for patient information sectioned (modified for loop)

--- treeview_design.py
+++ treeview_design.py
@@ -41,29 +41,68 @@ class Page1(tk.Frame):
         label.pack(pady=10,padx=10)
 
         c_columns = ["Patient Information", "Billing Information", "Insurance Information", "Insured Information (if other than patient"]
-
-        tree = ttk.Treeview(self, selectmode="browse", columns=c_columns)
-
-        tree.pack(fill="x")
-
-        for i in c_columns:
             #for loop for iterating all of the columns
+        #creating specific for loops for each section of the form
+        for column in c_columns:
+            if column == "Patient Information":
+                label = tk.Label(self, text=column, anchor='w')
+                label.pack(fill='x')
 
-            tree.heading(column=f'{i}',text=f'{i}',anchor='w')
-            tree.column(column=f'{i}', width=150, minwidth=50)
+                #creating label for "name" section
+                name_label = tk.Label(self, text="Name:")
+                name_label.pack(anchor='w', padx=10)
+
+                #creating title entry using combox,allowing user to select
+                title_label = tk.Label(self, text="Title")
+                title_label.pack(anchor='w', padx=10)
+                title_combobox = ttk.Combobox(self,
+                                              values=["Mr", "Ms", "Mrs", "Miss", "Master", "Madam", "Sir", "Other"])
+                title_combobox.pack(anchor='w', padx=10)
+                title_combobox.set("Select Title")
+
+                #creating entries under patient information
+                firstn_label = tk.Label(self, text="First Name")
+                firstn_label.pack(anchor='w', padx=10)
+                firstn_entry = tk.Entry(self)
+                firstn_entry.pack(anchor='w', padx=10)
+
+                middlen_label = tk.Label(self, text="Middle Name")
+                middlen_label.pack(anchor='w', padx=10)
+                middlen_entry = tk.Entry(self)
+                middlen_entry.pack(anchor='w', padx=10)
+
+                lastn_label = tk.Label(self, text="Last Name")
+                lastn_label.pack(anchor='w', padx=10)
+                lastn_entry = tk.Entry(self)
+                lastn_entry.pack(anchor='w', padx=10)
+
+                prefferedn_label = tk.Label(self, text="Preferred Name (optional)")
+                prefferedn_label.pack(anchor='w', padx=10)
+                prefferedn_entry = tk.Entry(self)
+                prefferedn_entry.pack(anchor='w', padx=10)
+
+                #creating label for adress section
+                address_label = tk.Label(self, text="Address:")
+                address_label.pack(anchor='w', padx=10)
+
+                #create entries for adress section here:
+
+            elif column == "Billing Information":
+                label = tk.Label(self, text=column, anchor='w')
+                label.pack(fill='x')
+
+                #add labels + entries for billing info here
+
+            else:
+                label = tk.Label(self, text=column, anchor='w')
+                label.pack(fill='x')
+
+                tk.Label(self, text="", height=1).pack()  # adding space between the labels/columns for design purposes/organization
+
+        button = tk.Button(self, text="Go to Page 2", command=lambda: controller.show_frame(Page2))
+        button.pack(side="bottom") #changed position of the button
 
         #tree.insert(parent="", 0, text="Title", values=())
-
-
-
-
-
-
-
-
-        button = tk.Button(self, text = "Go to Page 2", command = lambda: controller.show_frame(Page2))
-        button.pack()
-
 
 
 class Page2(tk.Frame):
@@ -77,20 +116,14 @@ class Page2(tk.Frame):
         label.pack(padx=10,pady=10)
         d_columns = ["Emergency Contact", "Parent/Guardian", "Medical Details"]
 
-        tree = ttk.Treeview(self, selectmode="browse", columns=d_columns)
+        for column in d_columns:
+            label = tk.Label(self, text=column, anchor='w')
+            label.pack(fill='x')
 
-        tree.pack(fill="x")
+            tk.Label(self, text="", height=1).pack()
 
-
-        for i in d_columns:
-            tree.heading(column=f'{i}', text=f'{i}', anchor='w')
-            tree.column(column=f'{i}', width=150, minwidth=50)
-
-        button = tk.Button(self, text = "Go to Page 1", command = lambda: controller.show_frame(Page1))
-        button.pack()
-
-
-
+        button = tk.Button(self, text="Go to Page 1", command=lambda: controller.show_frame(Page1))
+        button.pack(side="bottom")
 
 
 
